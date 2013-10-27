fdc07a62dbee29e4a560f6fc9f0367467ec09ec9
Karan Thukral <karanthukral@gmail.com>
2013-10-27 14:56:22 -0400

    New Glyphs

--- Remembr/collectionStore.m
+++ Remembr/collectionStore.m
@@ -36,7 +36,7 @@
 - (NSArray *)returnIconPack{
     NSMutableArray *iconParse = [[NSMutableArray alloc]init];
     
-    for(int i = 0; i < 79;i++){
+    for(int i = 0; i < 163;i++){
         
         NSString *imageNumber = [NSString stringWithFormat:@"%d",i];
         NSString *imageName = [imageNumber stringByAppendingString:@".png"];
